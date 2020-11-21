5ffdd8863d8d2886475c48ae0cd027db6836c59e
Dipankar jana <74833204+Dipankarjana@users.noreply.github.com>
2020-11-22 00:20:38 +0530

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,37 @@
+
+var employees = [];
+employees[0] = {
+
+name:"Nasim",
+age:"20",
+designation:"freelancer",
+knownLanguage:"java,javascript,andriodjava,swift",
+isactive:true,
+};
+employees[1] = {
+
+name:"rahul",
+age:"20",
+designation:"freelancer",
+knownLanguage:"java,javascript,andriodjava,swift",
+isactive:true,
+};
+employees[2] ={
+
+name:"prasenjit",
+age:"24",
+designation:"professer",
+knownLanguage:"java,javascript,andriodjava,swift",
+isactive:true,
+};
+employees[3] = {
+
+name:"sujit",
+age:"19",
+designation:"cooking",
+knownLanguage:"java,javascript,andriodjava,swift",
+isactive:true,
+};
+console.log(employees[0].name);
+console.log(employees[0].age);
+console.log(employees[0].knownLanguage);
